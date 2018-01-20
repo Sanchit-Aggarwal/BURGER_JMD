a07a0bf5a80476411f77d3c39bd42cf4bb59bb36
Sanchit-Aggarwal <sancool1995@gmail.com>
2018-01-20 18:33:48 +0530

    UI complete
    UI complete,functionality yet to be added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  addPatty()
+  {
+    alert();
+    document.getElementById('addON').innerHTML=<img src='/images/bottom-bun.jpg' />;
+  }
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='header'>
+            <div className='logo col-md-3'>LOGO WILL COME HERE</div>
+            <div className='title col-md-7'><h1>Mata Vaishno Burger point</h1></div>
+        </div>
+        <div className='Burger-Preview'>
+               <img src='/images/top-bun.png' height='150' width='300'/><br />
+               <div id='addON'></div>
+               <img src='/images/bottom-bun.jpg' />
+
+        </div>
+        <div className='adder jumbotron'>
+          <div className='item'>
+            Patty : <input type='text' /> &nbsp;
+          <button type="button" class="btn btn-primary" onClick={this.addPatty}>ADD TO BURGER</button>
+          </div>
+          <br />
+          <div className='item'>
+            Onion : <input type='text' /> &nbsp;
+          <button type="button" class="btn btn-primary">ADD TO BURGER</button>
+          </div>
+          <br />
+          <div className='item'>
+            Tomato  : <input type='text' /> &nbsp;
+          <button type="button" class="btn btn-primary">ADD TO BURGER</button>
+          </div>
+        </div>
+
       </div>
     );
   }
