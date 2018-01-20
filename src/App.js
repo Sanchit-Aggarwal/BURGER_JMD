544207db30d13b9d4b2935953aba9b320fc5c2e1
Sanchit-Aggarwal <sancool1995@gmail.com>
2018-01-21 00:04:39 +0530

    init load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+
+class Addon extends Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+  render()
+  {
+
+    return(
+      <div>
+
+                      <h1>I am an addon</h1>
+      </div>
+
+    );
+  }
+}
 
 class App extends Component {
   addPatty()
   {
     alert();
-    document.getElementById('addON').innerHTML=<img src='/images/bottom-bun.jpg' />;
+    ReactDOM.render(<Addon />,document.getElementById('addON'));
   }
   render() {
 
@@ -18,24 +38,27 @@ class App extends Component {
         </div>
         <div className='Burger-Preview'>
                <img src='/images/top-bun.png' height='150' width='300'/><br />
-               <div id='addON'></div>
+               <div id='addON'>
+
+
+               </div>
                <img src='/images/bottom-bun.jpg' />
 
         </div>
         <div className='adder jumbotron'>
           <div className='item'>
             Patty : <input type='text' /> &nbsp;
-          <button type="button" class="btn btn-primary" onClick={this.addPatty}>ADD TO BURGER</button>
+          <button type="button" className="btn btn-primary" onClick={this.addPatty}>ADD TO BURGER</button>
           </div>
           <br />
           <div className='item'>
             Onion : <input type='text' /> &nbsp;
-          <button type="button" class="btn btn-primary">ADD TO BURGER</button>
+          <button type="button" className="btn btn-primary">ADD TO BURGER</button>
           </div>
           <br />
           <div className='item'>
             Tomato  : <input type='text' /> &nbsp;
-          <button type="button" class="btn btn-primary">ADD TO BURGER</button>
+          <button type="button" className="btn btn-primary">ADD TO BURGER`</button>
           </div>
         </div>
 
