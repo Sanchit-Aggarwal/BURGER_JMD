326c71e9b3586c01c05baf09acb8b20e7e3e1c09
Sanchit-Aggarwal <sancool1995@gmail.com>
2018-01-21 16:09:35 +0530

    bugFix

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ return(<div>
                                         {
                                             if(t!=='')
                                             {
-                                            let img="/images/"+t+'.JPG'+"";
+                                            let img="public/images/"+t+'.JPG'+"";
                                             return(<div><img height='80' width='300' src={img} /><br /></div>);
                                           }
 
